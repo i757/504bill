e7376c514391b8fc3bda82ba703fdb6e7c0e12af
luoxiaozhu <328949691@qq.com>
2018-03-14 23:27:53 +0800

    .

--- js/app.js
+++ js/app.js
@@ -1,5 +1,40 @@
-var url = "localhost:8080/bill/";
+var url = "http://localhost:8080/bill/";
 function msg(msg){
     layer.msg(msg, {icon: 5});
     return false;
+}
+
+
+function ajaxRequest(url,method,data,callback) {
+    $.ajax({
+        url:url,
+        type:method,
+        data:JSON.stringify(data),
+        dataType:"json",
+        beforeSend:function (xhr){
+            xhr.setRequestHeader("Content-Type","application/json;charset=UTF-8");
+            layer.load();
+        },
+        success:callback,
+        error:function (xhr) {
+            getMessage(xhr.responseJSON.error.key);
+        },
+        complete:function (xhr,ts) {
+            layer.closeAll('loading');
+        }
+    });
+}
+
+function getMessage(key) {
+    $.getJSON('api.json',function (data) {
+        $.each(data,function (index,item) {
+            if(key === item.key){
+                layer.msg(item.message, {
+                    offset: 't',
+                    anim: 6
+                });
+                return;
+            }
+        })
+    })
 }
