b6397ea07f9d505cf02564f1ef555b6383a1e62b
luoxiaozhu <328949691@qq.com>
2018-03-26 15:38:03 +0800

    .

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-var url = "http://localhost:8080/bill/";
+var url = "http://www.joseph.xin/bill/";
 function msg(msg){
     layer.msg(msg, {icon: 5});
     return false;
@@ -17,6 +17,7 @@ function ajaxRequest(url,method,data,callback) {
         },
         success:callback,
         error:function (xhr) {
+
             getMessage(xhr.responseJSON.error.key);
         },
         complete:function (xhr,ts) {
@@ -37,4 +38,27 @@ function getMessage(key) {
             }
         })
     })
+}
+
+function getNowFormatDate() {
+    var date = new Date();
+    var seperator1 = "-";
+    var year = date.getFullYear();
+    var month = date.getMonth() + 1;
+    // var strDate = date.getDate();
+    if (month >= 1 && month <= 9) {
+        month = "0" + month;
+    }
+    // if (strDate >= 0 && strDate <= 9) {
+    //     strDate = "0" + strDate;
+    // }
+    // var currentdate = year + seperator1 + month + seperator1 + strDate;
+    var currentdate = year + seperator1 + month;
+    return currentdate;
+}
+
+function getClassName() {
+    var classArray = ["list-group-item-info","list-group-item-warning","list-group-item-danger","list-group-item-primary","list-group-item-success"];
+    var index = parseInt(Math.random() * classArray.length);
+    return classArray[index];
 }
